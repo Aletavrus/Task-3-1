cfc29e68a524fb4d436a7805f343fa3d2501a355
Aletavrus <152197218+Aletavrus@users.noreply.github.com>
2024-01-15 15:05:04 +0300

    Create Array in Main Program

--- Task 3-1/Program.cs
+++ Task 3-1/Program.cs
@@ -31,5 +31,88 @@ class HelloWorld
             int columns = int.Parse(Console.ReadLine());
             Two_Dimension two_Dimension = new Two_Dimension(creation_type, lines, columns);
         }
+        public static int[] Create_Single(int size, bool user_values)
+        {
+                int[] array = new int[size];
+                if (!user_values)
+                {
+                    Random random = new Random();
+                    for (int i=0; i<size; i++)
+                    {
+                        array[i] = random.Next(0, 100);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Enter a string with all values of an array separated by spaces");
+                    string input = Console.ReadLine();
+                    string[] input_lst = input.Split();
+                    for (int i = 0; i < size; i++)
+                    {
+                        array[i] = int.Parse(input_lst[i]);
+                    }
+                }
+            return array;
+        }
+        
+        public static int[][] Create_Many(int size, bool user_values)
+        {
+            if (!user_values)
+            {
+                Random random = new Random();
+                for (int i = 0; i < line; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        array[i, j] = random.Next(0, 100);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.WriteLine("Enter size of an inner array");
+                    int size = int.Parse(Console.ReadLine());
+                    array[i] = new int[size];
+                    Console.WriteLine("Enter values of 1 inner array in 1 string with spaces between elements");
+                    string input = Console.ReadLine();
+                    string[] input_lst = input.Split();
+                    for (int j = 0; j < array[i].Length; j++)
+                    {
+                        array[i][j] = int.Parse(input_lst[j]);
+                    }
+                }
+            }
+        }
+        
+        public static int[,] Create_Two(int line, int col, bool user_values)
+        {
+            if (!user_values)
+            {
+                Random random = new Random();
+                for (int i = 0; i < line; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        array[i, j] = random.Next(0, 100);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < line; i++)
+                {
+                    Console.WriteLine("Enter values of an array in 1 string with spaces between elements");
+                    string input = Console.ReadLine();
+                    string[] input_lst = input.Split();
+                    for (int j = 0; j < column; j++)
+                    {
+                        array[i, j] = int.Parse(input_lst[j]);
+                    }
+                }
+            }
+            return array;
+        }
     }
 }
